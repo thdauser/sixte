c384457a4ec15569c5681424a5f4a0de94d5d7f1
schmid <schmid@93064f36-db84-42fc-932c-92be5580b23f>
2009-02-25 16:11:57 +0000

    git-svn-id: file:///home/schmid/svn/sixt@63 93064f36-db84-42fc-932c-92be5580b23f

---
+++ src/imaging.h
@@ -0,0 +1,59 @@
+#ifndef IMAGING_H
+#define IMAGING_H 1
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <limits.h>
+//#include <gsl/gsl_randist.h>
+
+#include "fitsio.h"
+#include "pil.h"
+#include "headas.h"
+#include "headas_error.h"
+
+#include "sixt.h"
+#include "vector.h"
+//#include "spectrum.h"
+#include "check_fov.h"
+#include "fits_ctlg.h"
+//#include "imglib.h"
+#include "random.h"
+//#include "strftcpy.h"
+#include "detector.h"
+#include "psf.h"
+#include "photon.h"
+//#include "sources.h"
+#include "telescope.h"
+#include "orbatt.h"
+#include "measurement_array.h"
+
+
+#define TOOLSUB photon_imaging_main
+#include "headas_main.c"
+
+
+//#define MAX(x,y) ( (x)<=(y) ?(y) :(x) )
+//#define MIN(x,y) ( (x)<=(y) ?(x) :(y) )
+
+
+////////////////////////////////////////////////////////////////////////
+// Function declarations.
+////////////////////////////////////////////////////////////////////////
+
+
+// Reads the program parameters using PIL
+int photon_imaging_getpar(char orbit_filename[], char attitude_filename[], 
+			  char psf_filename[], double *t0, double *timespan, 
+			  struct Telescope *, struct Detector *);
+
+// Function performs a measurement for a specific source inside the FOV 
+// with given telescope data (direction of axis, direction of motion).
+int measure(struct Photon, struct Detector, struct Telescope, struct PSF_Store,
+	    struct Event_List_File* event_list_file);
+
+
+
+#endif
