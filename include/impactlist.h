bd7d51c8240122727a04a6eb32604b379ba595b8
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2009-07-09 11:28:39 +0200

    The FITS templates are now automatically installed to $pkgdatadir/templates.

--- include/impactlist.h
+++ include/impactlist.h
@@ -4,5 +4,19 @@
 #include "sixt.h"
 
 
+/** Structure that contains all information, which is necessary to access
+ * an impact list FITS file. */
+struct Impactlist_File {
+  fitsfile *fptr;
+
+  long row; /**< Current row in the table (starting at 0). */
+  long nrows; /**< Total number of rows in the table. */
+
+  /* Column numbers of the individual impact list entries. 
+   * The numbers start at 1. The number 0 means, that there 
+   * is no corresponding column in the table. */
+  int ctime, cenergy, cx, cy;
+};
+
 
 #endif /* IMPACTLIST_H */
