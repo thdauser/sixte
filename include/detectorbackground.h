d0bdabf05f3aeabd90ff124fadcd48615804e9e5
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2009-07-16 09:52:01 +0200

    Introduced new data structure to model the detector background.

---
+++ include/detectorbackground.h
@@ -0,0 +1,9 @@
+#ifndef DETECTORBACKGROUND_H
+#define DETECTORBACKGROUND_H 1
+
+typedef struct {
+  float rate; /**< Rate of detector background events. */
+} DetectorBackground;
+
+
+#endif /* DETECTORBACKGROUND */
