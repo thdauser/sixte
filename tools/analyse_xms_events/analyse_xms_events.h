bbb2eca61af88698481586627e55564e45a60ca4
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2009-08-14 12:47:12 +0200

    Added new tool for analysation of XMS event files. But the tools is not operational yet.

---
+++ tools/analyse_xms_events/analyse_xms_events.h
@@ -0,0 +1,33 @@
+#ifndef ANALYSE_XMS_EVENTS_H
+#define ANALYSE_XMS_EVENTS_H 1
+
+#include "sixt.h"
+#include "xmseventfile.h"
+
+
+#define TOOLSUB analyse_xms_events_main
+#include "headas_main.c"
+
+// Number of photons stored in the event list for each individual pixel.
+#define XMS_EVENT_LIST_ENTRIES 5
+
+
+struct Parameters{
+  /** Filename of the XMS event file. */
+  char eventlist_filename[MAXMSG];
+  /** Characteristic time unit of the TES microcalorimeter. */
+  double time_unit;
+  /** Time units before and after and event that may not be affected by further
+   * impacting photons in the same pixel. Otherwise the event energy determination
+   * will be degraded. */
+  int units_before_pulse, units_after_pulse;
+};
+
+
+//////////////////////////////////////////////////////////////////
+
+
+int analyse_xms_events_getpar(struct Parameters*);
+
+
+#endif /* ANALYSE_XMS_EVENTS_H */
