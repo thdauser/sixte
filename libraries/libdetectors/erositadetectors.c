a1b24dc91bdb6acf9e92862dd32853ff3eaa4903
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2009-09-28 14:00:49 +0200

    Added new data type 'eROSITADetector' representing the 7 CCDs of the eROSITA telescope.

---
+++ libraries/libdetectors/erositadetectors.c
@@ -0,0 +1,172 @@
+#include "erositadetectors.h"
+
+
+int initeROSITADetectors(eROSITADetectors* fd, 
+			 struct eROSITADetectorsParameters* parameters)
+{
+  int status = EXIT_SUCCESS;
+
+  // Call the initialization routines of the underlying data structures.
+  status = initGenericDetector(&fd->generic, &parameters->generic);
+  if (EXIT_SUCCESS!=status) return(status);
+  status = initSquarePixels(&fd->pixels, &parameters->pixels);
+  if (EXIT_SUCCESS!=status) return(status);
+
+  // Set up the erosita configuration.
+  fd->integration_time = parameters->integration_time;
+
+  // Set the first readout time such that the first readout is performed 
+  // immediately at the beginning of the simulation.
+  fd->readout_time = parameters->t0;
+  fd->frame = 0;
+
+  // Create and open new event list file.
+  status = openNeweROSITAEventFile(&fd->eventlist, parameters->eventlist_filename,
+				       parameters->eventlist_template);
+  if (EXIT_SUCCESS!=status) return(status);
+
+  return(status);
+}
+
+
+int cleanupeROSITADetectors(eROSITADetectors* fd) 
+{
+  int status=EXIT_SUCCESS;
+
+  // Call the cleanup routines of the underlying data structures.
+  cleanupSquarePixels(&fd->pixels);
+  status+=closeeROSITAEventFile(&fd->eventlist);
+
+  return(status);
+}
+
+
+int checkReadouteROSITADetectors(eROSITADetectors* fd, double time)
+{
+  int status = EXIT_SUCCESS;
+
+  // Check, if the detector integration time was exceeded. 
+  // In that case, read out the detector.
+  if (time > fd->readout_time) {
+    // It's time to read out the detector!
+      
+    // Readout the detector and create eventlist entries for the actual time:
+    status = readouteROSITADetectors(fd);
+    if (EXIT_SUCCESS!=status) return(status);
+
+    // Clear the detector array.
+    clearSquarePixels(&fd->pixels);
+
+    // Update the detector frame time to the next frame until the current
+    // time is within the detector->readout interval.
+    // This CAN ONLY BE DONE for EROSITA detectors!
+    // For detectors with individual readout lines a more complicated method is required.
+    while (time > fd->readout_time) {
+      fd->readout_time += fd->integration_time;
+      fd->frame += 2;
+    }
+
+    // Print the time of the current events (program status
+    // information for the user).
+    headas_printf("\rtime: %.3lf s ", fd->readout_time);
+    fflush(NULL);
+  }
+
+  return(status);
+}
+
+
+inline int readouteROSITADetectors(eROSITADetectors* fd) 
+{
+  int x, y;
+  int status = EXIT_SUCCESS;
+
+  // Read out the entire detector array.
+  for (x=0; x<fd->pixels.xwidth; x++) {
+    for (y=0; y<fd->pixels.ywidth; y++) {
+      if (fd->pixels.array[x][y].charge > 1.e-6) {
+	eROSITAEvent event;
+	// Determine the detector channel that corresponds to the charge stored
+	// in the detector pixel.
+	event.pha = getChannel(fd->pixels.array[x][y].charge, fd->generic.rmf);
+
+	// Check lower threshold (PHA and energy):
+	if ((event.pha>=fd->generic.pha_threshold) && 
+	    (fd->pixels.array[x][y].charge>=fd->generic.energy_threshold)) { 
+
+	  // REMOVE TODO
+	  assert(event.pha >= 0);
+	  // Maybe: if (event.pha < 0) continue;
+	
+	  // There is an event in this pixel, so insert it into the eventlist:
+	  event.time = fd->readout_time;
+	  event.energy = fd->pixels.array[x][y].charge * 1.e3; // [eV]
+	  event.xi = x;
+	  event.yi = y;
+	  event.frame = fd->frame;
+
+	  status=addeROSITAEvent2File(&fd->eventlist, &event);
+	  if (EXIT_SUCCESS!=status) return(status);
+	} // END of check for threshold
+      } // END of check whether  charge > 1.e-6
+    } // END of loop over y
+  } // END of loop over x
+
+  return(status);
+}
+
+
+int addImpact2eROSITADetectors(eROSITADetectors* fd, Impact* impact)
+{
+  int status=EXIT_SUCCESS;
+
+  // Before adding the new impact to the detector check whether
+  // a readout has to be performed in advance.
+  status=checkReadouteROSITADetectors(fd, impact->time);
+
+  // Determine a detector channel (PHA channel) according to the RMF.
+  // The channel is obtained from the RMF using the corresponding
+  // HEAdas routine which is based on drawing a random number.
+  long channel;
+  ReturnChannel(fd->generic.rmf, impact->energy, 1, &channel);
+
+  // Check if the photon is really measured. If the
+  // PHA channel returned by the HEAdas RMF function is '-1', 
+  // the photon is not detected.
+  // This can happen as the RMF is actually an RSP including, e.g., 
+  // the detector quantum efficiency and filter transmission.
+  if (-1==channel) {
+    return(status); // Break the function (and continue with the next photon).
+  }
+  assert(channel>=0);
+
+  // Get the corresponding created charge.
+  // NOTE: In this simulation the charge is represented by the nominal
+  // photon energy which corresponds to the PHA channel according to the
+  // EBOUNDS table.
+  float charge = getEnergy(channel, fd->generic.rmf);
+  
+  if (charge > 0.) {
+    int x[4], y[4];
+    double fraction[4];
+    
+    // Determine the affected detector pixels.
+    int npixels = getSquarePixelsSplits(&fd->pixels, &fd->generic, impact->position, 
+					x, y, fraction);
+    
+    // Add the charge created by the photon to the affected detector pixels.
+    int count;
+    for (count=0; count<npixels; count++) {
+      if (x[count] != INVALID_PIXEL) {
+	fd->pixels.array[x[count]][y[count]].charge += 
+	  charge * fraction[count];
+	  // |      |-> charge fraction due to split events
+	  // |-> charge created by incident photon
+      }
+    }
+  } // END if(charge>0.)
+
+  return(status);
+}
+
+
---
+++ libraries/libdetectors/erositadetectors.h
@@ -0,0 +1,84 @@
+#ifndef EROSITADETECTORS_H
+#define EROSITADETECTORS_H 1
+
+
+#include "sixt.h"
+#include "impact.h"
+#include "eventfile.h"
+#include "erositaeventfile.h"
+#include "erositaevent.h"
+#include "genericdetector.h"
+#include "squarepixels.h"
+
+
+////////////////////////////////////////////////////////////////////////
+// Type Declarations.
+////////////////////////////////////////////////////////////////////////
+
+
+typedef struct {
+  GenericDetector generic;
+  SquarePixels pixels;
+
+  /** Integration time of the entire pnCCD (!) detector array.
+   * (= Span of time between 2 subsequent readouts). */
+  double integration_time; 
+
+  /** Current readout time. The end of the integration 
+   * time / beginning of dead time. */
+  double readout_time; 
+
+  /** Number of the current frame. */
+  long frame; 
+
+  /** Event list FITS file for the eROSITA-specific events. */
+  eROSITAEventFile eventlist; 
+
+} eROSITADetectors;
+
+
+struct eROSITADetectorsParameters {
+  struct GenericDetectorParameters generic;
+  struct SquarePixelsParameters pixels;
+
+  double integration_time;
+  double t0;
+  char* eventlist_filename;
+  char* eventlist_template;  
+};
+
+
+/////////////////////////////////////////////////////////////////
+// Function Declarations.
+/////////////////////////////////////////////////////////////////
+
+
+/** Set up the configuration of a eROSITADetectors. 
+ * The routine also calls the init routines of the underlying data structures. */
+int initeROSITADetectors(eROSITADetectors*, struct eROSITADetectorsParameters*);
+
+/** Clean up the eROSITADetectors data structure. 
+ * Release allocated memory and call clean-up routines of underlying structures. */
+int cleanupeROSITADetectors(eROSITADetectors*);
+
+/** This routine is called for readout of the eROSITADetectors.
+ * The routine itself checks, whether a readout is necessary according to the current
+ * time and readout time. If it's time to do a readout, the routine initiates the 
+ * readout process by calling readouteROSITADetectors(). */
+int checkReadouteROSITADetectors(eROSITADetectors*, double time);
+
+/** Read out the eROSITADetectors.
+ * The measured events are stored in an event list FITS file. */
+inline int readouteROSITADetectors(eROSITADetectors*);
+
+/** Add a new photon impact to the eROSITADetectors pixels.
+ * The generated charge is determined according to the detector response.
+ * If the charge cloud size is set, split events are calculated according to
+ * a Gaussian charge cloud model.
+ * The new charge is added to the charge already contained in the detector pixel, 
+ * so pileup effects are taken into account. */
+int addImpact2eROSITADetectors(eROSITADetectors*, Impact*);
+
+
+#endif /* EROSITADETECTORS_H */
+
