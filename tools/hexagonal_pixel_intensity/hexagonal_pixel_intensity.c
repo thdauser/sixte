334a2320e5920aca71075ac8ea1db11aef70e748
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2009-10-02 10:27:20 +0200

    Analysation tool for the flux fraction on the hexagonal pixels of the HTRS performs now a loop over different pixel sizes.

--- tools/hexagonal_pixel_intensity/hexagonal_pixel_intensity.c
+++ tools/hexagonal_pixel_intensity/hexagonal_pixel_intensity.c
@@ -6,31 +6,49 @@
 
 int main(int argc, char* argv[])
 {
-  HexagonalPixels hexagonalPixels;
   ImpactListFile impactlistfile;
   Impact impact;
   int pixel; // Pixel hit by an incident photon.
 
-  long ntotal_photons; // Total number of photons.
-  long nphotons[37];   // Number of photons per pixel.
+  int count_pixelwidth;                // Counter for the different pixel widths.
+  const int n_pixelwidths=2;          // Number of different pixelwidths to simulate.
+  const double min_pixelwidth=0.5e-3;  // Minimum pixel width ([m]).
+  const double step_pixelwidth=0.5e-3; // Increment step for the pixel width ([m]).
+
+  HexagonalPixels hexagonalPixels[n_pixelwidths];
+
+  long ntotal_photons;                // Total number of photons.
+  long nphotons[n_pixelwidths][37];   // Number of photons per pixel.
 
   int status = EXIT_SUCCESS;
 
 
   if (argc < 2) {
-    printf("Error: have to specify impact list!\n");
+    printf("Error: You have to specify an impact list!\n");
     return(EXIT_FAILURE);
   }
 
   do { // Error handling loop 
 
-    // Initialization of HexagonalPixels data structure.
-    struct HexagonalPixelsParameters hpparameters = {
-      .npixels = 37,
-      .pixelwidth = 4.e-3  // in [m]
-    };
-    if(EXIT_SUCCESS!=(status=initHexagonalPixels(&hexagonalPixels, &hpparameters))) break;
+    // Loop over the different pixel widths.
+    for (count_pixelwidth=0; count_pixelwidth<n_pixelwidths; count_pixelwidth++) {
+      // Init buffers for statistical data.
+      for (pixel=0; pixel<37; pixel++) {
+	nphotons[count_pixelwidth][pixel] = 0;
+      }
 
+      // Init data structures for different pixel widths.
+      struct HexagonalPixelsParameters hpparameters = {
+	.npixels = 37,
+	.pixelwidth = min_pixelwidth + count_pixelwidth*step_pixelwidth  // [m]
+      };
+
+      // Initialization of HexagonalPixels data structure.
+      if(EXIT_SUCCESS!=(status=initHexagonalPixels(&hexagonalPixels[count_pixelwidth], 
+						   &hpparameters))) break;
+    } // END of loop over different pixel widths.
+    
+    
     // Open the impact list FITS file.
     status = openImpactListFile(&impactlistfile, argv[1], READONLY);
     if (EXIT_SUCCESS!=status) break;
@@ -43,40 +61,55 @@ int main(int argc, char* argv[])
       status=getNextImpactListFileRow(&impactlistfile, &impact);
       if (EXIT_SUCCESS!=status) break;
 
-      // Determine the pixel that is hit by the photon.
-      getHexagonalPixel(&hexagonalPixels, impact.position, &pixel);
+      // Loop over the different pixel widths.
+      for (count_pixelwidth=0; count_pixelwidth<n_pixelwidths; count_pixelwidth++) {
+	// Determine the pixel that is hit by the photon.
+	getHexagonalPixel(&hexagonalPixels[count_pixelwidth], impact.position, &pixel);
 
-      // Record data for statistics.
-      assert(pixel<37);
-      if (INVALID_PIXEL!=pixel) {
-	nphotons[pixel]++;
-      }
+	// Record data for statistics.
+	assert(pixel<37);
+	if (INVALID_PIXEL!=pixel) {
+	  nphotons[count_pixelwidth][pixel]++;
+	}
+      } // END of loop over different pixel widths.
 
     } // END of scanning the impact list.
 
-    // Release memory.
-    cleanupHexagonalPixels(&hexagonalPixels);
 
+    // Loop over the different pixel widths.
+    for (count_pixelwidth=0; count_pixelwidth<n_pixelwidths; count_pixelwidth++) {
+      // Determine the statistics and print them.
+      long ndetected=0;
+      double mean=0., mean2;
+      for (pixel=0; pixel<37; pixel++) {
+	ndetected += nphotons[count_pixelwidth][pixel];
+	mean2 += pow((double)nphotons[count_pixelwidth][pixel], 2.)/37.;
+      }
+      mean = ndetected*1./37.;
+      
+      printf("%lf %ld %ld %lf %lf\n",
+	     min_pixelwidth+count_pixelwidth*step_pixelwidth, // Pixel width
+	     ntotal_photons,                   // Total number of photons
+	     ndetected,                        // Number of detected photons
+	     ndetected*1./ntotal_photons,      // Fraction of detected photons
+	     sqrt(mean2-pow(mean,2.))/mean     // rms/mean
+	     );
 
-    // Determine the statistics and print them.
-    long ndetected=0;
-    double mean=0., mean2;
-    for (pixel=0; pixel<37; pixel++) {
-      ndetected += nphotons[pixel];
-      mean2 += pow((double)nphotons[pixel], 2.)/37.;
-    }
-    mean = ndetected*1./37.;
+    } // END of loop over different pixel widths.
 
-    printf("%lf %ld %lf %lf\n",
-	   hpparameters.pixelwidth, ntotal_photons,
-	   (double)ndetected/ntotal_photons, // fraction of detected photons
-	   sqrt(mean2-pow(mean,2.)) // rms
-	   );
-
-  } while (0); // END of error handling loop
+  } while (0); // END of error handling loop.
 
 
   // --- Clean up ---
+
+  // Close the impact list file.
+  status += closeImpactListFile(&impactlistfile);
+
+  // Loop over the different pixel widths.
+  for (count_pixelwidth=0; count_pixelwidth<n_pixelwidths; count_pixelwidth++) {
+    // Release memory.
+    cleanupHexagonalPixels(&hexagonalPixels[count_pixelwidth]);
+  } // END of loop over different pixel widths.
 
   return(status);
 }
