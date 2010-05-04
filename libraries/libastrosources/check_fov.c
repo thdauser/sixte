f20b2a5464e1d7974f1c6470e3b8c719256d4ea4
Christian <christian.schmid@sternwarte.uni-erlangen.de>
2010-05-04 12:53:09 +0200

    Cosmetic modifications.

--- libraries/libastrosources/check_fov.c
+++ libraries/libastrosources/check_fov.c
@@ -1,16 +1,21 @@
 #include "check_fov.h"
 
-/** Checks, whether the specified source x is inside the FOV.
- * The first check is,  whether the source is inside a circle, by <x,x0> < MIN_VAL.
- * The second and more closer check is carried out by the following formula:
- *
- * sin(theta_max) >= fabs(sin(theta)) = fabs(<r,h1>) = fabs(<x-x0,h1>) = 
- * fabs(<x,h1> - diff1) = fabs(<x,h1>)
- * (diff<i> = 0)
- *
- * Analogously with phi, h2 and diff2.
- *
- * The return value is 0, if the source is inside the FOV and >0 if it is outside. */
+/** Checks, whether the specified source x is inside the FOV.  The
+    first check is, whether the source is inside a circle, by 
+
+    <x,x0> < MIN_VAL.  
+
+    The second and more closer check is carried out by the following
+    formula:
+ 
+  sin(theta_max) >= fabs(sin(theta)) = fabs(<r,h1>) = fabs(<x-x0,h1>) = 
+  fabs(<x,h1> - diff1) = fabs(<x,h1>)
+  (diff<i> = 0)
+ 
+    Analogously with phi, h2 and diff2.
+ 
+    The return value is 0, if the source is inside the FOV and >0 if
+    it is outside. */
 inline int check_fov(Vector* const x, Vector* const x0, 
 		     /* Vector h1, Vector h2, double sin_dec_max, 
 			double sin_rasc_max, */ 
