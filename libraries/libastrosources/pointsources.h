0174d1190cfcb28fa86419190d9342e148f402e8
Christian Schmid <christian.schmid@sternwarte.uni-erlangen.de>
2010-05-12 11:52:24 +0200

    Removed obsolete PointSourceFileCatalog data structure.

--- libraries/libastrosources/pointsources.h
+++ libraries/libastrosources/pointsources.h
@@ -11,19 +11,27 @@
 #define MAX_N_POINTSOURCES 1000000
 
 
+////////////////////////////////////////////////////////////////////////
+// Type Declarations.
+////////////////////////////////////////////////////////////////////////
+
+
 /** Contains all data to specify the properties of a point source. */
 typedef struct {
   float ra, dec; /**< Right ascension and declination of the source [rad]. */
 
   /** Average photon rate [photons/s]. */
   float rate; 
-  /** Type of the light curve particular for this PointSource.
-   * There are different possible values:
-   * T_LC_CONSTANT (=0) means a constant light curve.
-   * T_LC_TIMMER_KOENIG (=-1) means a light curve with red noise according 
-   * to Timmer & Koenig (1995).
-   * Positive values can be used to designate a particular FITS file containing 
-   * a light curve. */
+  /** Type of the light curve particular for this PointSource. There
+      are different possible values:
+
+      * T_LC_CONSTANT (=0) means a constant light curve.
+
+      * T_LC_TIMMER_KOENIG (=-1) means a light curve with red noise
+        according to Timmer & Koenig (1995).
+
+      Positive values can be used to designate a particular FITS file
+      containing a light curve. */
   long lc_type;
   /** Pointer to object with Piece-wise linear light curve for this
       X-ray source. */
@@ -93,23 +101,10 @@ typedef struct {
 } PointSourceFile;
 
 
-/** Collection of point source FITS files. Used as input for the
-    point source selection algorithm. */
-typedef struct {
-  int nfiles;
-  PointSourceFile** files;
-} PointSourceFileCatalog;
-
-
 /////////////////////////////////////////////////////////////////
 // Function Declarations.
 /////////////////////////////////////////////////////////////////
 
-
-/** Constructor. */
-PointSourceFileCatalog* get_PointSourceFileCatalog();
-/** Destructor. */
-void free_PointSourceFileCatalog(PointSourceFileCatalog*);
 
 /** Constructor returning a pointer to an empty PointSourceFile object. */
 PointSourceFile* get_PointSourceFile();
@@ -130,16 +125,6 @@ void free_PointSourceFile(PointSourceFile*);
     PointSource objects. The selection is done based on a normal
     vector perpendicular to the scan plane and the cos(sin?)-value of
     the maximum angle between the source direction and the scan
-    plane. For each selected source a Spectrum and a Lightcurve are
-    assigned. */
-PointSourceCatalog* get_PointSourceCatalog(PointSourceFileCatalog*, Vector normal_vector, 
-					   const double max_align, int* status);
-
-/** Selects point sources along the path of the telescope axis over
-    the sky and returns a PointSourceCatalog containing the individual
-    PointSource objects. The selection is done based on a normal
-    vector perpendicular to the scan plane and the cos(sin?)-value of
-    the maximum angle between the source direction and the scan
     plane. To each selected source a spectrum and a light curve are
     assigned. */
 PointSourceCatalog* getPointSourceCatalog(PointSourceFile* psf, Vector normal_vector, 
@@ -148,19 +133,10 @@ PointSourceCatalog* getPointSourceCatalog(PointSourceFile* psf, Vector normal_ve
 /** Destructor. */
 void free_PointSourceCatalog(PointSourceCatalog* psc);
 
-/** Reads a table from a FITS source catalogue. 
- * The right ascension and declination of the return PointSource structure are 
- * given in [rad]. */
+/** Reads a table from a FITS source catalogue. The right ascension
+    and declination of the return PointSource structure are given in
+    [rad]. */
 int get_PointSourceTable_Row(PointSourceFile*, long row, PointSource*, int* status);
-
-/** Add a new PointSourceFile object to the PointSourceFileCatalog.
- * The function takes care of allocating enough memory,
- * loads the FITS HDU specified by the filename and the HDU number, and
- * adds it to the catalog.
- * The return value is the error status.
- */
-int addPointSourceFile2Catalog(PointSourceFileCatalog* psfc, char* filename, 
-			       int sources_hdu);
 
 
 #endif /* POINTSOURCES_H */
