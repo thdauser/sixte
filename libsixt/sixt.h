6f508bc81eb4d5c8fa04d07e44ab771cec7b09bc
Sebastian Falkner <Sebastian.Falkner@sternwarte.uni-erlangen.de>
2018-03-13 15:20:37 +0100

    Revert "[new] added CHECH_STATUS_BREAK_WITH_FITSERROR throwing the according FITS ERROR before breaking"
    This reverts commit e92cee7d23e780b8c486f4b96a3ecd19d1dfc4a2.

--- libsixt/sixt.h
+++ libsixt/sixt.h
@@ -100,16 +100,6 @@ extern char **sixt_argv;
 #define CHECK_STATUS_VOID(status) \
   if (EXIT_SUCCESS!=status) return;
 
-/** FITS error message for error handling macro retrieving error status */
-extern char _fits_err_msg[80];
-#define CHECK_STATUS_BREAK_WITH_FITSERROR(status) \
-  if (EXIT_SUCCESS!=status){ \
-	fits_get_errstatus(status,_fits_err_msg); \
-	SIXT_ERROR(_fits_err_msg); \
-	status=EXIT_FAILURE; \
-	break; \
-  }
-
 
 #define CHECK_NULL_VOID(a,status,msg) \
   if (NULL==a) { \
