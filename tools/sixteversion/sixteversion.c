16cb6f9621874737b9d94139e4c8ae8b584d5e97
Joern Wilms <joern.wilms@sternwarte.uni-erlangen.de>
2018-08-01 23:40:59 +0200

    added sixteversion command, changed maintainer email

---
+++ tools/sixteversion/sixteversion.c
@@ -0,0 +1,34 @@
+/*
+   This file is part of SIXTE.
+
+   SIXTE is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   SIXTE is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   For a copy of the GNU General Public License see
+   <http://www.gnu.org/licenses/>.
+
+
+   Copyright 2007-2018 Remeis-Sternwarte
+*/
+
+#include <sixteconfig.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+  printf("SIXTE version %s\n",PACKAGE_VERSION);
+  printf("Compiled %s, %s\n",__DATE__,__TIME__);
+  exit(0);
+}
+
+
+
