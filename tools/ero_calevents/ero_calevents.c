e05683134d560cbe21667b27d056cdf9b6d37fec
Thomas Dauser <Thomas.Dauser@sternwarte.uni-erlangen.de>
2019-01-11 11:32:25 +0100

    [ero_calevents] takes coord. trans. into account for attitude
    similar to the ccd rotation angle, the rollangle of the attitude written
    in the CORATT extension is now also converted to the erostia system
    (as seen from below towards the sky)

--- tools/ero_calevents/ero_calevents.c
+++ tools/ero_calevents/ero_calevents.c
@@ -46,13 +46,16 @@ int ero_calevents_main()
 
   // Register HEATOOL:
   set_toolname("ero_calevents");
-  set_toolversion("0.20");
+  set_toolversion("0.21");
 
 
   do { // Beginning of the ERROR handling loop (will at most be run once).
 
     // --- Initialization ---
 
+	char vers[1000];
+	get_toolnamev(vers);
+    headas_chat(3, " ** calling %s ** \n",vers);
     headas_chat(3, "initialization ...\n");
 
     // Read parameters using PIL library:
@@ -823,8 +826,8 @@ int ero_calevents_main()
       float ccdrotation;
       fits_read_key(elf->fptr, TFLOAT, "CCDROTA", &ccdrotation, comment, &status);
       if (EXIT_SUCCESS!=status) {
-	SIXT_ERROR("failed reading keyword CCDROTA in input file");
-	break;
+    	  SIXT_ERROR("failed reading keyword CCDROTA in input file");
+    	  break;
       }
 
       // Insert the data.
@@ -869,6 +872,17 @@ int ero_calevents_main()
 	  
 	  // Apply the rotation angle of the CCD.
 	  rollangle+=ccdrotation;
+	  // Important: Rollangle and CCD-Rotation
+	  // for eROSITA is given from below (towards the sky), and sixte from avoce (the mirror)
+	  // -> need to flip the direction in which the angles are defined
+	  rollangle = -rollangle;
+	  if (rollangle > 360.){
+		  rollangle -= 360.;
+	  }
+	  if (rollangle < -360.){
+		  rollangle += 360.;
+	  }
+
 	  
 	  // Store the data in the file.
 	  nrows++;
