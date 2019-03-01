thdauser/sixte

commit fa3b9ffcd4901decfc605d4552530edd4ad71a05

changes vignetting handling that energies are interpolated linearly

libsixt/vignetting.c
@@ -45,9 +45,15 @@ Vignetting* newVignetting(const char* const filename, int* const status)
     
     // Determine the column numbers of the individual columns.
     int column_energ_lo=0, column_energ_hi=0, column_theta=0;
+    int column_energy=0;
     int column_phi=0, column_vignet=0;
-    if(fits_get_colnum(fptr, CASEINSEN, "ENERG_LO", &column_energ_lo, status)) break;
-    if(fits_get_colnum(fptr, CASEINSEN, "ENERG_HI", &column_energ_hi, status)) break;
+
+    if(fits_get_colnum(fptr, CASEINSEN, "ENERGY", &column_energy, status)){
+        if(fits_get_colnum(fptr, CASEINSEN, "ENERG_LO", &column_energ_lo, status)) break;
+        if(fits_get_colnum(fptr, CASEINSEN, "ENERG_HI", &column_energ_hi, status)) break;
+    }
+
+
     if(fits_get_colnum(fptr, CASEINSEN, "THETA", &column_theta, status)) break;
     if(fits_get_colnum(fptr, CASEINSEN, "PHI", &column_phi, status)) break;
     if(fits_get_colnum(fptr, CASEINSEN, "VIGNET", &column_vignet, status)) break;
@@ -60,116 +66,127 @@ Vignetting* newVignetting(const char* const filename, int* const status)
 	   &vignetting->ntheta, &vignetting->nphi);
 
     // Allocate memory for the Vignetting data:
-    vignetting->energ_lo=(float*)malloc(vignetting->nenergies*sizeof(float));
-    vignetting->energ_hi=(float*)malloc(vignetting->nenergies*sizeof(float));
+    vignetting->energy  =(float*)malloc(vignetting->nenergies*sizeof(float));
     vignetting->theta   =(float*)malloc(vignetting->ntheta   *sizeof(float));
     vignetting->phi     =(float*)malloc(vignetting->nphi     *sizeof(float));
-    if ((NULL==vignetting->energ_lo) || (NULL==vignetting->energ_hi) ||
-	(NULL==vignetting->theta) || (NULL==vignetting->phi)) {
-      *status=EXIT_FAILURE;
-      SIXT_ERROR("could not allocate memory for storing the vignetting data");
-      break;
+    if ((NULL==vignetting->energy)  || (NULL==vignetting->theta) || (NULL==vignetting->phi)) {
+    	*status=EXIT_FAILURE;
+    	SIXT_ERROR("could not allocate memory for storing the vignetting data");
+    	break;
     } else {
-      for(count1=0; count1<vignetting->nenergies; count1++) {
-	vignetting->energ_lo[count1]=0.;
-	vignetting->energ_hi[count1]=0.;
-      }
-      for(count1=0; count1<vignetting->ntheta; count1++) {
-	vignetting->theta[count1]=0.;
-      }
-      for(count1=0; count1<vignetting->nphi; count1++) {
-	vignetting->phi[count1]=0.;
-      }
+    	for(count1=0; count1<vignetting->nenergies; count1++) {
+    		vignetting->energy[count1]=0.;
+    	}
+    	for(count1=0; count1<vignetting->ntheta; count1++) {
+    		vignetting->theta[count1]=0.;
+    	}
+    	for(count1=0; count1<vignetting->nphi; count1++) {
+    		vignetting->phi[count1]=0.;
+    	}
     }
 
     vignetting->vignet=(float***)malloc(vignetting->nenergies*sizeof(float**));
     if (NULL!=vignetting->vignet) {
-      for(count1=0; count1<vignetting->nenergies; count1++) {
-	vignetting->vignet[count1]=(float**)malloc(vignetting->ntheta*sizeof(float*));
-	if (NULL!=vignetting->vignet[count1]) {
-	  for(count2=0; count2<vignetting->ntheta; count2++) {
-	    vignetting->vignet[count1][count2]=
-	      (float*)malloc(vignetting->nphi*sizeof(float));
-	    if (NULL!=vignetting->vignet[count1][count2]) {
-	      for(count3=0; count3<vignetting->nphi; count3++) {
-		vignetting->vignet[count1][count2][count3]=0.;
-	      }
-	    } else {
-	      *status=EXIT_FAILURE;
-	      SIXT_ERROR("could not allocate memory for storing the vignetting data");
-	      break;
-	    }
-	  }
-	} else {
-	  *status=EXIT_FAILURE;
-	  SIXT_ERROR("could not allocate memory for storing the vignetting data");
-	  break;
-	}
-      }
+    	for(count1=0; count1<vignetting->nenergies; count1++) {
+    		vignetting->vignet[count1]=(float**)malloc(vignetting->ntheta*sizeof(float*));
+    		if (NULL!=vignetting->vignet[count1]) {
+    			for(count2=0; count2<vignetting->ntheta; count2++) {
+    				vignetting->vignet[count1][count2]=
+    						(float*)malloc(vignetting->nphi*sizeof(float));
+    				if (NULL!=vignetting->vignet[count1][count2]) {
+    					for(count3=0; count3<vignetting->nphi; count3++) {
+    						vignetting->vignet[count1][count2][count3]=0.;
+    					}
+    				} else {
+    					*status=EXIT_FAILURE;
+    					SIXT_ERROR("could not allocate memory for storing the vignetting data");
+    					break;
+    				}
+    			}
+    		} else {
+    			*status=EXIT_FAILURE;
+    			SIXT_ERROR("could not allocate memory for storing the vignetting data");
+    			break;
+    		}
+    	}
     } else {
-      *status=EXIT_FAILURE;
-      SIXT_ERROR("could not allocate memory for storing the vignetting data");
-      break;
+    	*status=EXIT_FAILURE;
+    	SIXT_ERROR("could not allocate memory for storing the vignetting data");
+    	break;
     }
     CHECK_STATUS_BREAK(*status);
 
     data_buffer=(float*)malloc(vignetting->nenergies*
-			       vignetting->ntheta*
-			       vignetting->nphi*sizeof(float));
+    		vignetting->ntheta*
+			vignetting->nphi*sizeof(float));
     if (NULL==data_buffer) {
-      *status=EXIT_FAILURE;
-      SIXT_ERROR("could not allocate memory for storing the vignetting data");
-      break;
+    	*status=EXIT_FAILURE;
+    	SIXT_ERROR("could not allocate memory for storing the vignetting data");
+    	break;
     } else {
-      for(count1=0; 
-	  count1<vignetting->nenergies*vignetting->ntheta*vignetting->nphi; 
-	  count1++) {
-	data_buffer[count1]=0.;
-      }
+    	for(count1=0;
+    			count1<vignetting->nenergies*vignetting->ntheta*vignetting->nphi;
+    			count1++) {
+    		data_buffer[count1]=0.;
+    	}
     }
     // Now all memory is allocated successfully!
 
-    
     // READ the data from the FITS table.
     int anynul=0;
-    fits_read_col(fptr, TFLOAT, column_energ_lo, 1, 1, vignetting->nenergies, 
-		  vignetting->energ_lo, vignetting->energ_lo, &anynul, status);
-    fits_read_col(fptr, TFLOAT, column_energ_hi, 1, 1, vignetting->nenergies, 
-		  vignetting->energ_hi, vignetting->energ_hi, &anynul, status);
+    if (column_energy){
+        fits_read_col(fptr, TFLOAT, column_energ_lo, 1, 1, vignetting->nenergies,
+        		vignetting->energy, vignetting->energy, &anynul, status);
+    } else {
+    	// if only ENERG_LO and ENERG_HI is given (old format), we need to calculate ENERGY
+        float* energ_lo=(float*)malloc(vignetting->nenergies*sizeof(float));
+        float* energ_hi=(float*)malloc(vignetting->nenergies*sizeof(float));
+
+        if (energ_lo==NULL || energ_hi==NULL){
+        	*status=EXIT_FAILURE;
+        	SIXT_ERROR("could not allocate memory for storing the vignetting data");
+        	break;
+        }
+
+        fits_read_col(fptr, TFLOAT, column_energ_lo, 1, 1, vignetting->nenergies,
+        		energ_lo, energ_lo, &anynul, status);
+        fits_read_col(fptr, TFLOAT, column_energ_hi, 1, 1, vignetting->nenergies,
+        		energ_hi, energ_hi, &anynul, status);
+
+        for (int ii=0; ii<vignetting->nenergies; ii++){
+        	vignetting->energy[ii] = 0.5*(energ_lo[ii]+energ_hi[ii]);
+        }
+        free(energ_lo);
+        free(energ_hi);
+    }
+
+
     fits_read_col(fptr, TFLOAT, column_theta, 1, 1, vignetting->ntheta, 
-		  vignetting->theta, vignetting->theta, &anynul, status);
+    		vignetting->theta, vignetting->theta, &anynul, status);
     fits_read_col(fptr, TFLOAT, column_phi, 1, 1, vignetting->nphi, 
-		  vignetting->phi, vignetting->phi, &anynul, status);
+    		vignetting->phi, vignetting->phi, &anynul, status);
     fits_read_col(fptr, TFLOAT, column_vignet, 1, 1, 
-		  vignetting->nenergies*vignetting->ntheta*vignetting->nphi, 
-		  data_buffer, data_buffer, &anynul, status);
+    		vignetting->nenergies*vignetting->ntheta*vignetting->nphi,
+			data_buffer, data_buffer, &anynul, status);
 
     // Determine the minimum and maximum available energy:
-    vignetting->Emin=-1.;
-    vignetting->Emax=-1.;
-    for (count1=0; count1<vignetting->nenergies; count1++) {
-      if ((vignetting->energ_lo[count1]<vignetting->Emin) || (vignetting->Emin<0.)) {
-	vignetting->Emin=vignetting->energ_lo[count1];
-      }
-      if (vignetting->energ_hi[count1]>vignetting->Emax) {
-	vignetting->Emax=vignetting->energ_hi[count1];
-      }
-    }						
+    vignetting->Emin=vignetting->energy[0];
+    vignetting->Emax=vignetting->energy[vignetting->nenergies-1];
+
     // Scale from [deg] -> [rad]:
     for (count1=0; count1<vignetting->ntheta; count1++) {
-      vignetting->theta[count1]*=M_PI/180.;
+    	vignetting->theta[count1]*=M_PI/180.;
     }
     for (count1=0; count1<vignetting->nphi; count1++) {
-      vignetting->phi[count1]*=M_PI/180.;
+    	vignetting->phi[count1]*=M_PI/180.;
     }
 
     // Plot debug information about available energies, off-axis
     // angles, and azimuthal angles.
     headas_chat(5, "Vignetting - available energies:\n");
     for (count1=0; count1<vignetting->nenergies; count1++) {
-      headas_chat(5, " %.3lf keV - %.3lf keV\n", 
-		  vignetting->energ_lo[count1],
-		  vignetting->energ_hi[count1]);
+      headas_chat(5, " %.3lf keV  \n",
+          vignetting->energy[count1]);
     }
     headas_chat(5, "Vignetting - available off-axis angles:\n");
     for (count1=0; count1<vignetting->ntheta; count1++) {
@@ -192,9 +209,9 @@ Vignetting* newVignetting(const char* const filename, int* const status)
 
 	  // Output of the vignetting value for this particular parameters.
 	  headas_chat(5, "Vignetting: %.2lf%% for "
-		      "%.1lf keV - %.1lf keV, %.4lf arc min, %.4lf deg, \n", 
+		      "%.1lf keV, %.4lf arc min, %.4lf deg, \n",
 		      vignetting->vignet[count1][count2][count3]*100., 
-		      vignetting->energ_lo[count1], vignetting->energ_hi[count1],
+		      vignetting->energy[count1],
 		      vignetting->theta[count2]/M_PI*180.*60.,
 		      vignetting->phi[count3]/M_PI*180.);
 	}
@@ -216,8 +233,7 @@ Vignetting* newVignetting(const char* const filename, int* const status)
 
 void destroyVignetting(Vignetting** const vi) {
   if (NULL!=*vi) {
-    if (NULL!=(*vi)->energ_lo) free((*vi)->energ_lo);
-    if (NULL!=(*vi)->energ_hi) free((*vi)->energ_hi);
+    if (NULL!=(*vi)->energy) free((*vi)->energy);
     if (NULL!=(*vi)->theta)    free((*vi)->theta);
     if (NULL!=(*vi)->phi)      free((*vi)->phi);
     
@@ -242,7 +258,7 @@ void destroyVignetting(Vignetting** const vi) {
 
 
 // At the moment SIXTE can only handle the cases without phi dependece phi = 0.
-static float get_vign_ener(const float theta, const float* arr_theta, float** arr_vign, const int ntheta){
+static float interpol_vign_theta(const float theta, const float* arr_theta, float** arr_vign, const int ntheta){
 
 
 	// Check if the required angle is larger than the biggest in the
@@ -282,44 +298,27 @@ float get_Vignetting_Factor(const Vignetting* const vi, const float energy,
   }
   */
 
-  if ((energy<vi->Emin) || (energy>vi->Emax)) {
-    return(-1.); // Energy is out of range!
-  } 
-  
+  /* if we are above or below the defined energies, we return the vignetting
+   * value at the edge of the grid  */
+  if (energy<=vi->Emin){
+	  return interpol_vign_theta(theta, vi->theta, vi->vignet[0], vi->ntheta);
+  }
+  if (energy>=vi->Emax){
+	  return interpol_vign_theta(theta, vi->theta, vi->vignet[vi->nenergies-1], vi->ntheta);
+  }
+
   // Find the right energy bin.
-  // assume linear interpolation at middle of the bin
-  int ii;
-  int ind_bin = 0;
-  float vign_val[2];
-  for(ii=0; ii<vi->nenergies; ii++) {
-	  if ((energy>=vi->energ_lo[ii])&&(energy<=vi->energ_hi[ii])) {
-		  float ener_mean = 0.5* (vi->energ_lo[ii] + vi->energ_hi[ii]);
+  int ind = binary_search_float(energy,vi->energy,vi->nenergies );
 
-		  // if below first grid point: return first grid point
-		  if (ii==0 && energy < ener_mean ){
-			  return get_vign_ener(theta, vi->theta, vi->vignet[ii], vi->ntheta);
-		  }
+  double vign_val[2];
+  // assume linear interpolation
+  vign_val[0] = interpol_vign_theta(theta, vi->theta, vi->vignet[ind], vi->ntheta);
+  vign_val[1] = interpol_vign_theta(theta, vi->theta, vi->vignet[ind+1], vi->ntheta);
 
 
+  double ifac = (energy-vi->energy[ind]) / ( vi->energy[ind+1] - vi->energy[ind] );
 
-		  // if above last grid point: return last grid point
-		  if (ii==vi->nenergies-1 && energy > ener_mean ){
-			  return get_vign_ener(theta, vi->theta, vi->vignet[ii], vi->ntheta);
-		  }
+  return interp_lin_1d(ifac, vign_val[0], vign_val[1]);
 
-
-		  // we're in the first energy bin
-		  if (energy < 0.5*(e)==0){
-			  vign_val = get_vign_ener(theta, vi->theta, vi->vignet[ii], vi->ntheta);
-
-		  }
-
-		  return vign_val;
-	  }
-  } 
-  // END of loop to find the right energy bin.
-
-  assert(ii<vi->nenergies);
-  return(0.); // (this should never be reached)
 }
 

libsixt/sixt.c
@@ -786,6 +786,10 @@ void fits_close_file_chksum(fitsfile *fptr,int *status) {
   fits_close_file(fptr,status);
 }
 
+/** linear interpolation in 1 dimension **/
+double interp_lin_1d(double ifac_r, double rlo, double rhi){
+	return ifac_r*rhi + (1.0-ifac_r)*rlo;
+}
 
 void sixt_check_obsolete_keyword(int* status){
 

libsixt/sixt.h
@@ -392,5 +392,7 @@ int binary_search_long(double val, double* arr, long n);
 int binary_search_float(float val, float* arr, int n);
 int binary_search_float_long(float val, float* arr, long n);
 
+double interp_lin_1d(double ifac_r, double rlo, double rhi);
+
 #endif /* SIXT_H */
 

libsixt/vignetting.h
@@ -38,10 +38,8 @@ typedef struct {
   /** Number of azimuth angles. */
   int nphi; 
 
-  /** Minimum energy of bin in [keV]. */
-  float* energ_lo;
-  /** Maximum energy of bin in [keV]. */ 
-  float* energ_hi;
+  /** Mean energy of bin in [keV] (0.5*(energ_lo+energ_hi). */
+  float* energy;
   /** Off-axis angle in [rad]. */
   float* theta; 
   /** Azimuthal angle in [rad]. */

test/unit/Makefile.am
@@ -5,12 +5,13 @@ LOG_DRIVER = env AM_TAP_AWK='$(AWK)' $(SHELL) \
                   $(top_srcdir)/build-aux/tap-driver.sh
 
 # Try to do a proper Test setup with cmocka
-check_PROGRAMS = unit_test_all random_number_gen test_genpixgrid
-TESTS = unit_test_all random_number_gen test_genpixgrid
+check_PROGRAMS = unit_test_all random_number_gen test_genpixgrid test_vignetting
+TESTS = unit_test_all random_number_gen test_genpixgrid test_vignetting
 
 unit_test_all_LDFLAGS = -lcmocka
 random_number_gen_LDFLAGS = -lcmocka
 test_genpixgrid_LDFLAGS = -lcmocka
+test_vignetting_LDFLAGS = -lcmocka
 
 
 random_number_gen_LDADD =@top_builddir@/libsixt/libsixt.la

test/unit/data/dummy_vign.fits
@@ -1,0 +1,1 @@
+SIMPLE  =                    T / file does conform to FITS standard             BITPIX  =                   16 / number of bits per data pixel                  NAXIS   =                    0 / number of data axes                            EXTEND  =                    T / FITS dataset may contain extensions            COMMENT   FITS (Flexible Image Transport System) format is defined in 'AstronomyCOMMENT   and Astrophysics', volume 376, page 359; bibcode: 2001A&A...376..359H END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             XTENSION= 'BINTABLE'           / binary table extension                         BITPIX  =                    8 / 8-bit bytes                                    NAXIS   =                    2 / 2-dimensional binary table                     NAXIS1  =                   64 / width of table in bytes                        NAXIS2  =                    1 / number of rows in table                        PCOUNT  =                    0 / size of special data area                      GCOUNT  =                    1 / one data group (required keyword)              TFIELDS =                    6 / number of fields in each row                   TTYPE1  = 'ENERG_LO'           / label for field   1                            TFORM1  = '2E      '           / data format of field: 4-byte REAL              TTYPE2  = 'ENERG_HI'           / label for field   2                            TFORM2  = '2E      '           / data format of field: 4-byte REAL              TTYPE3  = 'ENERGY  '           / label for field   3                            TFORM3  = '2E      '           / data format of field: 4-byte REAL              TTYPE4  = 'THETA   '           / label for field   4                            TFORM4  = '3E      '           / data format of field: 4-byte REAL              TTYPE5  = 'PHI     '           / label for field   5                            TFORM5  = '1E      '           / data format of field: 4-byte REAL              TTYPE6  = 'VIGNET  '           / label for field   6                            TFORM6  = '6E      '           / data format of field: 4-byte REAL              EXTNAME = 'VIGNET  '           / name of this binary table extension            TDIM1   = '(2)     '                                                            TDIM2   = '(2)     '                                                            TDIM3   = '(2)     '                                                            TDIM4   = '(3)     '                                                            TDIM5   = '(1)     '                                                            TDIM6   = '(2,3,1) '                                                            HDUCLASS= 'OGIP    '                                                            HDUCLAS1= 'RESPONSE'                                                            HDUVERS1= '1.0.0   '                                                            HDUCLAS2= 'VIGNET  '                                                            HDUVERS2= '1.1.0   '                                                            VERSION = '20190228'                                                            MISSION = 'none    '                                                            TELESCOP= 'none    '                                                            DETNAM  = 'none    '                                                            INSTRUME= 'none    '                                                            TUNIT1  = 'keV     '                                                            TUNIT2  = 'keV     '                                                            TUNIT3  = 'keV     '                                                            TUNIT4  = 'degree  '                                                            TUNIT5  = 'degree  '                                                            END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 @@  @@  A       @       =ªª«?                   ?€  =‰Ù;Ù²                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                

test/unit/test_vignetting.c
@@ -1,0 +1,29 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+//#include "sixt.h"
+#include "vignetting.h"
+
+const char* = "data/dummy_vign.fits";
+
+
+
+static int test_vign_load(int seed){
+  
+  return status;
+}
+
+
+int main(void)
+{
+  
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_vign_load),
+  };
+
+  cmocka_set_message_output(CM_OUTPUT_TAP);
+
+  return cmocka_run_group_tests_name("Default",tests,NULL,NULL);
+}

commit e5fb8c67aeeada6a2b9c76c88683dca1fae23c89

fixes problem of reading older vign files introduced by fa3b9ff

libsixt/vignetting.c
@@ -48,6 +48,10 @@ Vignetting* newVignetting(const char* const filename, int* const status)
     int column_phi=0, column_vignet=0;
 
     if(fits_get_colnum(fptr, CASEINSEN, "ENERGY", &column_energy, status)){
+        headas_chat(5, "Column ENERGY in the vignetting file %s doesn't exist, using ENERG_LO and ENERG_HI instead \n", filename);
+        headas_chat(7, "  ... resetting cfitsio error stack\n");
+        fits_clear_errmsg();
+        *status = EXIT_SUCCESS;
         if(fits_get_colnum(fptr, CASEINSEN, "ENERG_LO", &column_energ_lo, status)) break;
         if(fits_get_colnum(fptr, CASEINSEN, "ENERG_HI", &column_energ_hi, status)) break;
     }

test/exec/erosim.csh
@@ -21,7 +21,7 @@ erosim  \
   XMLFile7=${xml}/erosita_7.xml \
   Simput=$indir/simput.fits \
   Exposure=1 \
-  clobber=yes 
+  clobber=yes chatter=5
 
 set num_wc = `ls output/erosim*_evt.fits* | wc -l`
 if ($num_wc != 7) then